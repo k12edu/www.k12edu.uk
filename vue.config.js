272b6971ce1d4d971411584161b0dc7ac9a06939
Howpig2003 <howpig.coder@gmail.com>
2024-11-27 12:12:51 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/' // 如果是Github無字定義網域，就要放存儲庫名稱
+    ? '/' // 如果是Github無字定義網域，就要放存儲庫名稱 
     : '/'
 }
