31eaaa4920cfbcd446d45edaac8ee3e328c06cba
Howpig2003 <howpig.coder@gmail.com>
2024-12-25 20:54:31 +0800

    seprate subjectitem

--- src/components/HomePage/HomePage.vue
+++ src/components/HomePage/HomePage.vue
@@ -1,19 +1,26 @@
 <template>
     <HeaderItem @moveToObject="moveToObject"></HeaderItem>
     <SubjectItem id="subject-div1" :Title="title[0]" :idx=1></SubjectItem>
-    <SubjectItem id="subject-div2" :Title="title[1]" :idx=2></SubjectItem>
-    <SubjectItem id="subject-div3" :Title="title[2]" :idx=3></SubjectItem>
-    <SubjectItem id="subject-div4" :Title="title[3]" :idx=4></SubjectItem>
+    <SubjectItem2 id="subject-div2" :Title="title[1]" :idx=2></SubjectItem2>
+    <SubjectItem3 id="subject-div3" :Title="title[2]" :idx=3></SubjectItem3>
+    <SubjectItem4 id="subject-div4" :Title="title[3]" :idx=4></SubjectItem4>
   </template>
   
   <script>
   import HeaderItem from '../HeaderItem.vue';
   import SubjectItem from '../SubjectItem.vue';
+  import SubjectItem2 from '../SubjectItem2.vue';
+  import SubjectItem3 from '../SubjectItem3.vue';
+  import SubjectItem4 from '../SubjectItem4.vue';
+
   export default{
     name: 'HomePage',
     components:{
         HeaderItem,
-        SubjectItem
+        SubjectItem,
+        SubjectItem2,
+        SubjectItem3,
+        SubjectItem4
     },
     data(){
       return{
---
+++ src/components/SubjectItem4.vue
@@ -0,0 +1,125 @@
+<template>
+    <div class="subject-item" :style="subjectStyle">
+      <div class="gray-background"></div>
+      <div class="main-div">
+        <div class="left-div">
+          <h1 style="color: white;">{{ Title }}</h1>
+          <div class="main-item">
+            <h2>主要功能</h2>
+            <p>利用模型本身能力與知識庫，引導學生解決學習時遇到的困難。</p>
+          </div>
+          <div class="main-item">
+            <h2>原始模型與訓練</h2>
+            <p>基於 yentinglin/Llama-3-Taiwan-8B-Instruct 模型，針對數學、自然科學、資訊工程、常識進行微調訓練</p>
+          </div>
+          <div class="main-item">
+            <h2>使用的開源庫</h2>
+            <p>LLaMA-Factory(模型訓練), ollama(模型運行), ragflow(rag知識庫)</p>
+          </div>
+        </div>
+        <div class="right-div">
+          <div class="part-img"></div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default{
+    name: 'HeaderItem',
+    components:{
+  
+    },
+    data(){
+      
+    },
+    props:{
+      Title:String,
+      idx:Number,
+    },
+    computed:{
+      subjectStyle(){
+        return {
+          backgroundImage: `url(${require(`../assets/img${this.idx}.jpg`)})`, // 修正引號
+          backgroundSize: 'cover', // 如果需要覆蓋整個元素
+          backgroundPosition: 'center' // 如果需要置中顯示
+        };
+      }
+    },
+    methods:{
+      
+    }
+  }
+  </script>
+  
+  <style scoped>
+    .part-img{
+      height: 60%;
+      width: 100%;
+      border: 4px solid white;
+      margin-top: 10%;
+    }
+    .right-div{
+      z-index: 1;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      height: 100%;
+      width: 80%;
+    }
+    .left-div{
+      z-index: 1;
+      display: flex;
+      flex-direction: column;
+      align-items: start;
+      height: 100%;
+      width: 80%;
+    }
+    .main-item{
+      display: flex;
+      flex-direction: column;
+      align-items: start;
+      height: 30%;
+      width: 50%;
+      margin: 20px 0px;
+      color: white;
+      text-overflow: ellipsis;
+      overflow: hidden;
+    }
+    .main-div{
+      z-index: 1;
+      display: flex;
+      justify-content: space-between;
+      height: 90%;
+      width: 90%;
+    }
+    .subject-item{
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+      margin: 0;
+      
+    }
+    .gray-background{
+      position: absolute;
+      height: 100vh;
+      width: 100%;
+      background-color: rgba(0, 0, 0, 0.5);
+    }
+    p{
+      text-wrap: wrap;
+      text-align: left;
+    }
+    h2{
+      margin-bottom: 0px;
+    }
+    @media(max-width:700px){
+      .right-div{
+        display: none;
+      }
+      .main-item{
+        width: 70%;
+      }
+    }
+  </style>
